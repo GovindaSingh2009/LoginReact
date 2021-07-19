291949a1579af233c523411f312e3121fa4e8bef
G <govinda.g@vodafone.com>
2021-07-19 21:23:47 +0200

    my project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
+import Mypic from './Mypic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Mypic} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Govinda .welcome to react world!
         </p>
         <a
           className="App-link"
